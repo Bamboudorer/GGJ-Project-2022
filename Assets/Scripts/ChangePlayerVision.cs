4a274e3b2a02c78bc8fa0ff9b0a3303eab3e92be
Bamboudorer <lucas.techer@epitech.eu>
2022-01-30 03:23:21 +0400

    Add an object displayed with the new power

--- Assets/Scripts/ChangePlayerVision.cs
+++ Assets/Scripts/ChangePlayerVision.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ChangePlayerVision : MonoBehaviour
 {
     [SerializeField] private GameObject redVision;
+    [SerializeField] private LayerMask normalMask;
+    [SerializeField] private LayerMask poweredMask;
+    [SerializeField] private Camera persoCamera;
     private float maxTimePower = 10;
     private float timeActivePower = 10;
     private bool asActivePower = false;
@@ -12,7 +15,7 @@ public class ChangePlayerVision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,16 +23,25 @@ public class ChangePlayerVision : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (asActivePower) {
-                Debug.Log("Disable power, back to normal");
-                asActivePower = false;
-                redVision.SetActive(false);
-            }
-            else {
-                Debug.Log("Change Color, Activate power");
-                asActivePower = true;
-                redVision.SetActive(true);
-            }
+            ChangeActivePower();
+        }
+    }
+
+    private void ChangeActivePower()
+    {
+        if (asActivePower)
+        {
+            Debug.Log("Disable power, back to normal");
+            asActivePower = false;
+            redVision.SetActive(false);
+            persoCamera.cullingMask = normalMask;
+        }
+        else
+        {
+            Debug.Log("Change Color, Activate power");
+            asActivePower = true;
+            redVision.SetActive(true);
+            persoCamera.cullingMask = poweredMask;
         }
     }
 }
